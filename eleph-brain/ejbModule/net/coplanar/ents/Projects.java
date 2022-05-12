a7866680f29d20b48d73542ae2cf7c89bc4e0696
Jeremy Jackson <jerj@coplanar.net>
2022-05-12 07:42:11 -0400

    add Users and Projects tables

---
+++ eleph-brain/ejbModule/net/coplanar/ents/Projects.java
@@ -0,0 +1,40 @@
+package net.coplanar.ents;
+
+import java.io.Serializable;
+import java.lang.String;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Projects
+ *
+ */
+@Entity
+
+public class Projects implements Serializable {
+
+	   
+	@Id
+	@GeneratedValue ( strategy = GenerationType.IDENTITY )
+	private int proj_id;
+	private String pname;
+	private static final long serialVersionUID = 1L;
+
+	public Projects() {
+		super();
+	}   
+	public int getProj_id() {
+		return this.proj_id;
+	}
+
+	public void setProj_id(int proj_id) {
+		this.proj_id = proj_id;
+	}   
+	public String getPname() {
+		return this.pname;
+	}
+
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+   
+}
---
+++ eleph-brain/ejbModule/net/coplanar/ents/Users.java
@@ -0,0 +1,40 @@
+package net.coplanar.ents;
+
+import java.io.Serializable;
+import java.lang.String;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Users
+ *
+ */
+@Entity
+
+public class Users implements Serializable {
+
+	   
+	@Id
+	@GeneratedValue ( strategy = GenerationType.IDENTITY )
+	private int user_id;
+	private String username;
+	private static final long serialVersionUID = 1L;
+
+	public Users() {
+		super();
+	}   
+	public int getUser_id() {
+		return this.user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}   
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+   
+}
