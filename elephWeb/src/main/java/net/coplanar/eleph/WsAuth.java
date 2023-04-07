311c1442badc924e1b7c5b1b04e5a7e7775a7449
Jeremy Jackson <jerj@coplanar.net>
2023-04-07 08:38:02 -0400

    add UPN to HttpSession for use later

--- elephWeb/src/main/java/net/coplanar/eleph/WsAuth.java
+++ elephWeb/src/main/java/net/coplanar/eleph/WsAuth.java
@@ -2,6 +2,7 @@ package net.coplanar.eleph;
 
 import java.util.ArrayList;
 
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.http.HttpSession;
 import jakarta.websocket.HandshakeResponse;
 import jakarta.websocket.server.HandshakeRequest;
@@ -21,8 +22,18 @@ public void modifyHandshake(ServerEndpointConfig sec, HandshakeRequest request, 
     	response.getHeaders().put(HandshakeResponse.SEC_WEBSOCKET_ACCEPT, new ArrayList<String>());
         System.out.println("modifyHandshake null UPN");
     }
-    // fork a thread to preload user
+    // https://stackoverflow.com/questions/17936440/accessing-httpsession-from-httpservletrequest-in-a-web-socket-serverendpoint
+    HttpSession httpSession = (HttpSession)request.getHttpSession();
+//    sec.getUserProperties().put(HttpSession.class.getName(),httpSession);
+    sec.getUserProperties().put("theHttpSession",httpSession);
+    // this is likely gettable somewhere already
+    httpSession.setAttribute("theUPN",upn);
+    
+    // fork a thread to preload user - investigate using Futures to do this
+    // maybe start a JPA Extended Transaction on new httpSession?
 }
 }
 
 // how to asynchronously get user from database... promise resolved in first onMessage?
+// https://stackoverflow.com/questions/17936440/accessing-httpsession-from-httpservletrequest-in-a-web-socket-serverendpoint
+// https://stackoverflow.com/questions/20240591/websocket-httpsession-returns-null
